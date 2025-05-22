86fc003e22835a897f1ad4f574f7f991f1e802f0
Swyftl <juniorjonswift@gmail.com>
2025-05-22 22:49:29 +0100

    Added new Vec3 to the engine

--- FeatherEngine/Rectangle.cs
+++ FeatherEngine/Rectangle.cs
@@ -2,22 +2,29 @@
 
 namespace FeatherEngine;
 
-public class Rectangle(int x, int y, int width, int height) : IRenderable
+public class Rectangle : IRenderable
 {
+    private Vec2 Position;
+    private Vec2 Scale;
+
+    public Rectangle(Vec2 Position, Vec2 Scale)
+    {
+        this.Position = Position;
+        this.Scale = Scale;
+    }
+    
     public void Draw()
     {
-        Raylib.DrawRectangle(x, y, width, height, Color.Red);
+        Raylib.DrawRectangle((int)Position.X, (int)Position.Y, (int)Scale.X, (int)Scale.Y , Color.Red);
     }
 
     public void Change_Position(int newX, int newY)
     {
-        x = newX;
-        y = newY;
+        this.Position = new Vec2(newX, newY);
     }
 
     public void Change_Size(int newWidth, int newHeight)
     {
-        width = newWidth;
-        height = newHeight;
+        this.Scale = new Vec2(newWidth, newHeight);
     }
 }
---
+++ FeatherEngine/Vec2.cs
@@ -0,0 +1,57 @@
+﻿namespace FeatherEngine;
+
+public struct Vec2
+{
+    public float X { get; set; }
+    public float Y { get; set; }
+
+    public Vec2(float x, float y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    // Vector addition
+    public static Vec2 operator +(Vec2 a, Vec2 b)
+    {
+        return new Vec2(a.X + b.X, a.Y + b.Y);
+    }
+
+    // Vector subtraction
+    public static Vec2 operator -(Vec2 a, Vec2 b)
+    {
+        return new Vec2(a.X - b.X, a.Y - b.Y);
+    }
+
+    // Scalar multiplication
+    public static Vec2 operator *(Vec2 a, float scalar)
+    {
+        return new Vec2(a.X * scalar, a.Y * scalar);
+    }
+
+    // Scalar division
+    public static Vec2 operator /(Vec2 a, float scalar)
+    {
+        return new Vec2(a.X / scalar, a.Y / scalar);
+    }
+
+    // Magnitude (Length)
+    public float Magnitude()
+    {
+        return (float)Math.Sqrt(X * X + Y * Y);
+    }
+
+    // Normalize
+    public Vec2 Normalize()
+    {
+        float magnitude = Magnitude();
+        if (magnitude > 0)
+            return this / magnitude;
+        return new Vec2(0, 0);
+    }
+
+    public override string ToString()
+    {
+        return $"({X}, {Y})";
+    }
+}
