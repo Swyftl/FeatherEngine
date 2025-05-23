79fea84e84ffa2af00127edbf74a8997a479f735
Swyftl <juniorjonswift@gmail.com>
2025-05-23 01:08:17 +0100

    Added Input.cs

---
+++ FeatherEngine/Input.cs
@@ -0,0 +1,33 @@
+﻿using Raylib_cs;
+
+namespace FeatherEngine;
+
+public class Input(KeyboardKey key)
+{
+    private KeyboardKey InputKey = key;
+    private bool was_key_down = false;
+
+    public bool IsKeyDown()
+    {
+        if (Raylib.IsKeyDown(InputKey))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public bool IsKeyUp()
+    {
+        if (Raylib.IsKeyUp(InputKey))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
