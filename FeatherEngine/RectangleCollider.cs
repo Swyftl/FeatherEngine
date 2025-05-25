b2e213ce2d3c0b7a59bf9874deae10f3fd6900bc
Swyftl <juniorjonswift@gmail.com>
2025-05-25 18:54:42 +0100

    Added ToDo to the rect collider

--- FeatherEngine/RectangleCollider.cs
+++ FeatherEngine/RectangleCollider.cs
@@ -5,6 +5,7 @@ namespace FeatherEngine;
 
 public class RectangleCollider(int x, int y, int width, int height)
 {
+    // ToDo - Make passive collisions with other objects
     public bool checkClick()
     {
         var mousePosition = Raylib.GetMousePosition();
