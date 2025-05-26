7ed75475da586ae3359afeb4927fdfa885a00ff7
Swyftl <juniorjonswift@gmail.com>
2025-05-26 01:33:14 +0100

    Added NodeManager.cs for managing the nodes and rendering them on the screen properly

---
+++ FeatherEngine/Managers/NodeManager.cs
@@ -0,0 +1,38 @@
+﻿namespace FeatherEngine.Managers;
+
+public class NodeManager
+{
+    private List<INode> _nodes = new List<INode>();
+    private IntPtr _renderer;
+
+    public NodeManager(IntPtr renderer)
+    {
+        this._renderer = renderer;
+    }
+    
+    public void AddNode(INode node)
+    {
+        _nodes.Add(node);
+    }
+
+    public void RemoveNode(INode node)
+    {
+        _nodes.Remove(node);
+    }
+
+    public void RenderNodes()
+    {
+        foreach (var node in _nodes)
+        {
+            node.Render(_renderer);
+        }
+    }
+
+    public void OnQuit()
+    {
+        foreach (var node in _nodes)
+        {
+            node.Destroy();
+        }
+    }
+}
